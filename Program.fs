8cb2ed764e227a98928091f1daa1a654c6dece9a
subcogitate <jgstringer@gmail.com>
2018-04-05 22:09:11 -0500

    A random fruit generator. Arrays

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let RandomFruits count = 
+    let fruits = [|"apple"; "pear" ; "banana"; "orange"; "cantelope"|]
+    let r = Random()
+    [|
+        for i in 1..count do
+            let index = r.Next(5)
+            yield fruits.[index]    
+    |]
+
+[<EntryPoint>]
+let main argv =
+    for x in argv do
+        printfn "%A" (RandomFruits (x |> int))
+
+    0 // return an integer exit code
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FSharpFundamentals
 learning fsharp for fun and profit
+
+This will probably have a few projects in here.
